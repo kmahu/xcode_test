bad185b8f6630d0efdf1a33b81da0f39fb749b5a
kmahu <kirti.mahurkar@gmail.com>
2017-03-30 18:43:43 -0500

    first commit kirti

---
+++ multi_time_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multi_time_test
+//
+//  Created by Kirti Mahurkar on 3/28/17.
+//  Copyright © 2017 Kirti Mahurkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
